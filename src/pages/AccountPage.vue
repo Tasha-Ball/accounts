2473b621f524cfabdab5162f197e0571554a88ef
Tasha-Ball <nplotnikova565@gmail.com>
2025-02-25 14:59:10 +0500

    AccountPage

---
+++ src/pages/AccountPage.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useAccountStore } from '@/store/useAccountStore'
+import Button from "primevue/button"
+
+const store = useAccountStore()
+</script>
+
+<template>
+  <div class="account">
+    <div class="account__title">
+      <h1>Учетные записи</h1>
+      <Button severity="secondary" variant="outlined" @click="store.addAccount">
+        <i class="pi pi-plus"></i>
+      </Button>
+    </div>
+    <span class="account__hint">
+      <i class="pi pi-info-circle"></i>
+      Для указания нескольких меток для одной пары логин/пароль используйте разделить ;
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.account {
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+.account__title {
+  display: flex;
+  gap: 20px;
+}
+.account__title h1 {
+  font-size: 1.5rem;
+}
+.account__hint {
+  background-color: #f1f5f9;
+  padding: 5px;
+  border-radius: 6px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AccountPage from '@/pages/AccountPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: AccountPage,
+    },
+  ],
 })
 
 export default router
